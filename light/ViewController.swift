691123fb4727a738a642e39f8c12335898d0f10c
Getu Gebre <getugebre@getus-macbook-air.local>
2019-01-28 14:47:00 -0600

    Initial Commit

---
+++ light/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  light
+//
+//  Created by Getu Gebre on 1/28/19.
+//  Copyright © 2019 Getu Gebre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
+    @IBOutlet weak var lightButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
